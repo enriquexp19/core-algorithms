6bfed53c2c92c991f0c227f3ba9964ff5eb40a78
Aaron Villanueva <aaronvillanueva@Aarons-MacBook-Pro.local>
2017-03-13 14:02:31 -0700

    isPalindrome

--- test/fizzBuzz_test.js
+++ test/fizzBuzz_test.js
@@ -7,7 +7,25 @@ describe("fizzBuzz()", function() {
     const output = fizzBuzz(5)
     expect(output).to.deep.equal([1, 2, "Fizz", 4, "Buzz"])
   })
-  console.log(fizzBuzz(15), '********************************')
+
+  it("should return an array of numbers from 1 to 15", function(){
+    const output = fizzBuzz(15)
+    expect(output).to.deep.equal([ 1, 2,
+  'Fizz',
+  4,
+  'Buzz',
+  'Fizz',
+  7,
+  8,
+  'Fizz',
+  'Buzz',
+  11,
+  'Fizz',
+  13,
+  14,
+  'fizzBuzz' ])
+  })
+
   it("should return error if input is invalid", function() {
     const output = fizzBuzz('potato')
     expect(output).to.equal('error')
