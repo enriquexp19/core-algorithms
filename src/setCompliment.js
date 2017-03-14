460dced84817e1e77dd47aca0f765f430c4182cf
Aaron Villanueva <aaronvillanueva@Aarons-MacBook-Pro.local>
2017-03-14 11:45:52 -0700

    setCompliment

---
+++ src/setCompliment.js
@@ -0,0 +1,22 @@
+export default function setCompliment(a, b) {	
+	var array = [],
+	if (typeof a !== 'object' || typeof b !== 'object') {
+		return 'error'
+	}
+	else if (a.length !== b.length) {
+		return 'not the same length'
+	}
+	for (var i = 0; i < a.length; i++) {
+		function matches(element) {
+		if (element === a[i]) return element
+		}
+		if (b.find(matches) !== undefined) {
+			array.push(b.find(matches))
+		}
+	}
+	for (var i = 0; i < array.length; i++) {
+		var pos = b.indexOf(array[i])
+		b.splice(pos, 1)
+	}
+	return b
+}
--- src/setIntersection.js
+++ src/setIntersection.js
@@ -6,7 +6,7 @@ export default function setIntersection(a, b) {
 	for (var i = 0; i < a.length; i++) {
 		function matches(element) {
 		if (element === a[i]) return element
-	}
+		}
 		if (b.find(matches) !== undefined) {
 			array.push(b.find(matches))
 		}
